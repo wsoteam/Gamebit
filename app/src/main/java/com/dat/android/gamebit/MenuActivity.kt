d251096c14b11ac814ed865539344e3251d96a71
Wadim <81143156+PolinaYak@users.noreply.github.com>
2021-04-27 17:34:57 +0300

    aadd

--- app/src/main/java/com/dat/android/gamebit/MenuActivity.kt
+++ app/src/main/java/com/dat/android/gamebit/MenuActivity.kt
@@ -1,10 +1,19 @@
 package com.dat.android.gamebit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity(R.layout.activity_menu) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        tv_button_settings_from_menu.setOnClickListener {
+            var intentSetting = Intent (this@MenuActivity, SettingActivity::class.java)
+            startActivity(intentSetting)
+        }
     }
+
 }
