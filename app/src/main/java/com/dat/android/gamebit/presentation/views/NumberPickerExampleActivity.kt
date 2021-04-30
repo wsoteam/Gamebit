789db59bd5270c52f156999c0b3888057a73a4cb
Wadim <40245987+vsalamov@users.noreply.github.com>
2021-04-30 13:06:51 +0300

    add number picker

---
+++ app/src/main/java/com/dat/android/gamebit/presentation/views/NumberPickerExampleActivity.kt
@@ -0,0 +1,7 @@
+package com.dat.android.gamebit.presentation.views
+
+import androidx.appcompat.app.AppCompatActivity
+import com.dat.android.gamebit.R
+
+class NumberPickerExampleActivity : AppCompatActivity(R.layout.number_picker_example_activity) {
+}
