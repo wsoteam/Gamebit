ddb9ac94b1282b2c7d0c1b6fcbab30b8d4720231
Wadim <81143156+PolinaYak@users.noreply.github.com>
2021-04-30 13:45:41 +0300

    addd

--- app/src/main/java/com/dat/android/gamebit/FragmentDialog.kt
+++
@@ -1,6 +0,0 @@
-package com.dat.android.gamebit
-
-import androidx.fragment.app.Fragment
-
-class FragmentDialog: Fragment() {
-}
---
+++ app/src/main/java/com/dat/android/gamebit/presentation/game/FragmentDialogDefeat.kt
@@ -0,0 +1,8 @@
+package com.dat.android.gamebit.presentation.game
+
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+
+class FragmentDialogDefeat: DialogFragment() {
+
+}
