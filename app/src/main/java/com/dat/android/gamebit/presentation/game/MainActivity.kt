ea1d36919b89f8014d355b20b09290fb4b1a055c
Wadim <40245987+vsalamov@users.noreply.github.com>
2021-04-30 13:35:32 +0300

    add attr number picker

--- app/src/main/java/com/dat/android/gamebit/presentation/game/MainActivity.kt
+++ app/src/main/java/com/dat/android/gamebit/presentation/game/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.dat.android.gamebit.presentation.game
 
 import android.animation.ValueAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.dat.android.gamebit.PreferenceProvider
 import com.dat.android.gamebit.R
+import com.shawnlin.numberpicker.NumberPicker
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_menu.*
+import java.lang.String
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         updateUI()
@@ -19,6 +24,20 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             ivRoulete.rotation = it.animatedValue.toString().toFloat()
         }
         anim.start()
+        bindSpinners()
+    }
+
+    private fun bindSpinners() {
+        val data =
+            arrayOf("$50", "$100", "$200", "$300", "$400", "$500", "$1000", "$1500", "$2000")
+        npBet.minValue = 1
+        npBet.maxValue = data.size
+        npBet.displayedValues = data
+        npBet.value = 3
+        /// "kek,lol,kek,kek" -- split --> array("kek", "lol", "kek", "kek") --> array[1] == "lol"
+        /// "$50" -- split("$") --- array( "", "50") --- array[1].toInt
+
+        var value = data[0].split("$")[1].toInt()
     }
 
     private fun updateUI() {
