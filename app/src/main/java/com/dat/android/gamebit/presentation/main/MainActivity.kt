13d9b6d70ec51440f92f1b452dcbbd1dc57bea2f
Wadim <81143156+PolinaYak@users.noreply.github.com>
2021-05-03 13:37:36 +0300

    add
    selecton btn black red

--- app/src/main/java/com/dat/android/gamebit/presentation/main/MainActivity.kt
+++ app/src/main/java/com/dat/android/gamebit/presentation/main/MainActivity.kt
@@ -14,33 +14,41 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    var hash = hashMapOf<Int, Float>( Pair(34, 5f), )
-    var redList = arrayListOf(34, 27, )
+   // var hash = hashMapOf<Int, Float>( Pair(34, 5f) )
+   // var redList = arrayListOf(34, 27 )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         updateUI()
         bindSpinners()
+        bindSpinnersNumbers()
+        pressBlackButton()
+        pressRedButton()
+
+
 
         iv_show_highscoresActivity.setOnClickListener {
             var intentHigh = Intent(this@MainActivity, HighscoresActivity::class.java)
             startActivity(intentHigh)
         }
 
+
+
+
         /*tv_button_play.setOnClickListener {
             var dialogDefeat = FragmentDialogDefeat()
             dialogDefeat.show(supportFragmentManager, "customDialog")
         }*/ // диалог
 
-        var animator = ValueAnimator.ofFloat(0f, 360f)
+        /*var animator = ValueAnimator.ofFloat(0f, 360f)
         animator.addUpdateListener {
             ivRoulete.rotation = it.animatedValue.toString().toFloat()
         }
-        animator.start()
+        animator.start()*/
 
-        var value = Random.nextInt(5, 10)
-        var amountRotations = value * 360f
+       // var value = Random.nextInt(5, 10)
+      //  var amountRotations = value * 360f
 
 
     }
@@ -57,6 +65,35 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         var value = data[0].split("$")[1].toInt()
     }
+
+    private fun bindSpinnersNumbers() {
+        val num =
+            arrayOf("1", "2", "3", "4", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+                "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+                "30", "31", "32", "33", "34", "35", "36", "0" )
+
+        npNumber.minValue = 1
+        npNumber.maxValue = num.size
+        npNumber.displayedValues = num
+        npNumber.value = 3
+
+        var value = num[1]
+    }
+
+   private fun pressBlackButton() {
+
+       tv_button_black.setOnClickListener {
+           tv_button_black.isSelected = true
+           tv_button_red.isSelected = false
+       }
+    }
+    private fun pressRedButton() {
+        tv_button_red.setOnClickListener {
+            tv_button_black.isSelected = false
+            tv_button_red.isSelected = true
+        }
+    }
+
 
     private fun updateUI() {
         var backState = PreferenceProvider.getBackgroundStateSetting()
