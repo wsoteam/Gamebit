fd0970241e1739575b2e91fdc639dfc5bf6b2ee1
Wadim <81143156+PolinaYak@users.noreply.github.com>
2021-05-11 14:30:02 +0300

    add
    urlmaker(split strings)

--- app/src/main/java/com/dat/android/gamebit/presentation/SplashActivity.kt
+++ app/src/main/java/com/dat/android/gamebit/presentation/SplashActivity.kt
@@ -11,9 +11,43 @@ import android.util.Log
 import com.dat.android.gamebit.presentation.menu.MenuActivity
 import com.dat.android.gamebit.R
 import com.dat.android.gamebit.utils.URLMaker
+import com.dat.android.gamebit.utils.URLMaker.createMonkissLanding
+import com.dat.android.gamebit.utils.URLMaker.createMonkissRegF
+import com.dat.android.gamebit.utils.URLMaker.createMonkissRegSimple1
+import com.dat.android.gamebit.utils.URLMaker.createMonkissRegSimple2
+import com.dat.android.gamebit.utils.URLMaker.createTCLanding
+import com.dat.android.gamebit.utils.URLMaker.createTCRegBP
+import com.dat.android.gamebit.utils.URLMaker.createTCRegF
+import com.dat.android.gamebit.utils.URLMaker.createTCRegS
+import com.dat.android.gamebit.utils.URLMaker.createVulLanding
+import com.dat.android.gamebit.utils.URLMaker.createVulRegFast
+import com.dat.android.gamebit.utils.URLMaker.createWPLanding
+import com.dat.android.gamebit.utils.URLMaker.createWPRegBP
+import com.dat.android.gamebit.utils.URLMaker.createWPRegFast
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
+
+
+    companion object{
+        const val WELCOME_PARTNERS_LANDING = "wpl"
+        const val WELCOME_PARTNERS_REG_FAST= "wprf"
+        const val WELCOME_PARTNERS_REG_BONUS_PAGE = "wprb"
+
+        const val VULKAN_LANDING = "vl"
+        const val VULKAN_REG_FAST = "vrf"
+
+        const val TRAFFIC_CAKE_LANDING = "tcl"
+        const val TRAFFIC_CAKE_REG_SIMPLE = "tcrs"
+        const val TRAFFIC_CAKE_REG_FAST = "tcrf"
+        const val TRAFFIC_CAKE_REG_BONUS_PAGE = "tcrbp"
+
+        const val MONKISS_LANDING = "ml"
+        const val MONKISS_REG_FAST = "mrf"
+        const val MONKISS_REG_SIMPLE_1 = "mrs1"
+        const val MONKISS_REG_SIMPLE_2 = "mrs2"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var span = SpannableString(getString(R.string.splash_title))
@@ -21,11 +55,29 @@ class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
 
         tvTitle.text = span
 
-        var url = "wpl_[landing id]_[webmaster id]_[program id]_[subid]"
+        var url = "mrs1_[site id]_[webmaster id]_[program id]_[subid]"
+
         var key = url.split("_")[0]
-        when(key){
-            "wpl" -> createURlWelcomePar()
+        var outputURL = ""
+        outputURL = when(key){
+            WELCOME_PARTNERS_LANDING -> createWPLanding(url, "https://luckyland777.com/")
+            WELCOME_PARTNERS_REG_FAST -> createWPRegFast(url, "https://mobyslot7.com/")
+            WELCOME_PARTNERS_REG_BONUS_PAGE -> createWPRegBP(url,"https://mobyslot7.com/")
+            VULKAN_LANDING -> createVulLanding(url, "https://vulkantop.pro/")
+            VULKAN_REG_FAST -> createVulRegFast(url, "https://aff-redir.com/")
+            TRAFFIC_CAKE_LANDING -> createTCLanding(url, "https://cakeglobaloffers.com/")
+            TRAFFIC_CAKE_REG_SIMPLE -> createTCRegS(url, "https://sweet-slots.com/")
+            TRAFFIC_CAKE_REG_FAST -> createTCRegF(url, "https://sweet-slots.com/")
+            TRAFFIC_CAKE_REG_BONUS_PAGE -> createTCRegBP(url, "https://sweet-slots.com/")
+            MONKISS_LANDING -> createMonkissLanding(url, "https://12monkiredirect.com/")
+            MONKISS_REG_FAST -> createMonkissRegF(url, "https://777todayspin-redirect.com/")
+            MONKISS_REG_SIMPLE_1 -> createMonkissRegSimple1(url, "https://777todayspin-redirect.com/")
+            MONKISS_REG_SIMPLE_2 -> createMonkissRegSimple2(url, "https://777todayspin-redirect.com/")
+
+            else -> "hui"
         }
+        Log.e("LOL", outputURL)
+
 
         intent = Intent(this@SplashActivity, MenuActivity::class.java)
         startActivity(intent)
--- app/src/main/java/com/dat/android/gamebit/utils/URLMaker.kt
+++ app/src/main/java/com/dat/android/gamebit/utils/URLMaker.kt
@@ -3,14 +3,152 @@ package com.dat.android.gamebit.utils
 object URLMaker {
 
     //todayspinl_[landing id]_[webmaster id]_[program id]_[subid]
-    //https://mobyslot7.com/?s=[site id]&ref=wp_w[webmaster id]p[program id]_[subid]&url=register
-    fun createWPLanding(naming : String, domain : String) : String{
+    // https://luckyland777.com/[landing id]/?ref=wp_w[webmaster id]p[program id]_[subid]&click_id={click_id}
+
+    //wl_[landing id]_[webmaster id]_[program id]_[subid]
+
+    fun createWPLanding(naming: String, domain: String): String {
+        //https://luckyland777.com/[landing id]/?ref=wp_w[webmaster id]p[program id]_[subid]
         var landingId = naming.split("_")[1]
         var wm = naming.split("_")[2]
-        var programmId =  naming.split("_")[3]
-        var subId =  naming.split("_")[4]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}${landingId}/?ref=wp_w${wm}p${programmId}_${subId}"
+
+        return url
+    }
+
+    fun createWPRegFast(naming: String, domain: String): String {
+        //https://mobyslot7.com/?s=[site id]&ref=wp_w[webmaster id]p[program id]_[subid]&url=register
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=wp_w${wm}p${programmId}_${subId}&url=register"
+
+        return url
+    }
+    fun createWPRegBP(naming: String, domain: String): String {
+        //https://mobyslot7.com/?s=[site id]&ref=wp_w[webmaster id]p[program id]_[subid]&encoded_url=Ym9udXNlcy9saXN0I3BvcHVwLXJlZw=
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=wp_w${wm}p${programmId}_${subId}&encoded_url=Ym9udXNlcy9saXN0I3BvcHVwLXJlZw="
+
+        return url
+    }
+    fun createVulLanding(naming: String, domain: String): String {
+        //https://vulkantop.pro/landingpages/[landing id]/index.php?ref=va_w[webmaster id]p[program id]_[subid]
+        var landingId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}landingpages/${landingId}/index.php?ref=va_w${wm}p${programmId}_${subId}"
+
+        return url
+    }
+    fun createVulRegFast(naming: String, domain: String): String {
+        //https://aff-redir.com/?s=[site id]&ref=va_w[webmaster id]p[program id]_[subid]&url=register
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=va_w${wm}p${programmId}_${subId}&url=register"
+
+        return url
+    }
+    fun createTCLanding(naming: String, domain: String): String {
+        //https://cakeglobaloffers.com/landingpages/[landing id]/index.php?ref=ca_w[webmaster id]p[program id]_[subid]
+        var landingId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}landingpages/${landingId}/index.php?ref=ca_w${wm}p${programmId}_${subId}"
+
+        return url
+    }
+    fun createTCRegS(naming: String, domain: String): String {
+        //https://sweet-slots.com/?s=[site id]&ref=ca_w[webmaster id]p[program id]_[subid]&encoded_url=IyEvYXV0aC9yZWdpc3Rlcg==
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=ca_w${wm}p${programmId}_${subId}&encoded_url=IyEvYXV0aC9yZWdpc3Rlcg=="
+
+        return  url
+    }
+    fun createTCRegF(naming: String, domain: String): String {
+        //https://sweet-slots.com/?s=[site id]&ref=ca_w[webmaster id]p[program id]_[subid]&encoded_url=cmVnaXN0ZXI=
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=ca_w${wm}p${programmId}_${subId}&encoded_url=cmVnaXN0ZXI="
+
+        return url
+    }
+    fun createTCRegBP(naming: String, domain: String): String {
+        //https://sweet-slots.com/?s=[site id]&ref=ca_w[webmaster id]p[program id]_[subid]&encoded_url=Ym9udXNlcyMhL2F1dGgvcmVnaXN0ZXI=
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=ca_w${wm}p${programmId}_${subId}&encoded_url=Ym9udXNlcyMhL2F1dGgvcmVnaXN0ZXI="
+
+        return url
+    }
+    fun createMonkissLanding(naming: String, domain: String): String {
+        //https://12monkiredirect.com/landingpages/[landing id]/index.php?ref=mk_w[webmaster id]p[program id]_[subid]
+        var landingId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
 
         var url = "${domain}landingpages/${landingId}/index.php?ref=mk_w${wm}p${programmId}_${subId}"
+
+        return url
+    }
+    fun createMonkissRegF(naming: String, domain: String): String {
+        //https://777todayspin-redirect.com/?s=[site id]&ref=mk_w[webmaster id]p[program id]_[subid]&encoded_url=cmVnaXN0ZXI=
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=mk_w${wm}p${programmId}_${subId}&encoded_url=cmVnaXN0ZXI="
+
+        return url
+    }
+    fun createMonkissRegSimple1(naming: String, domain: String): String {
+        //https://777todayspin-redirect.com/?s=[site id]&ref=mk_w[webmaster id]p[program id]_[subid]&encoded_url=IyEvYXV0aC9yZWdpc3Rlcg==
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}?s=${siteId}&ref=mk_w${wm}p${programmId}_${subId}&encoded_url=IyEvYXV0aC9yZWdpc3Rlcg=="
+
+        return url
+    }
+    fun createMonkissRegSimple2(naming: String, domain: String): String {
+        //https://777todayspin-redirect.com/?s=[site id]&ref=mk_w[webmaster id]p[program id]_[subid]&encoded_url=IyEvYXV0aC9yZWc=
+        var siteId = naming.split("_")[1]
+        var wm = naming.split("_")[2]
+        var programmId = naming.split("_")[3]
+        var subId = naming.split("_")[4]
+
+        var url = "${domain}/?s=${siteId}&ref=mk_w${wm}p${programmId}_${subId}&encoded_url=IyEvYXV0aC9yZWc="
+
         return url
     }
 }
