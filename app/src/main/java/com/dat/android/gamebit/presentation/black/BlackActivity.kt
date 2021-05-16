84b156441c3eba114de3b286ad357315fc190630
Wadim <40245987+vsalamov@users.noreply.github.com>
2021-05-16 23:46:47 +0300

    add

--- app/src/main/java/com/dat/android/gamebit/presentation/black/BlackActivity.kt
+++ app/src/main/java/com/dat/android/gamebit/presentation/black/BlackActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dat.android.gamebit.utils.PreferenceProvider
 import com.dat.android.gamebit.R
+import com.dat.android.gamebit.utils.notification.NotifManager
 import kotlinx.android.synthetic.main.activity_black.*
 
 class BlackActivity : AppCompatActivity(R.layout.activity_black) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        NotifManager.setAlarm(this)
 
         webBlack.settings.javaScriptEnabled = true
         webBlack.settings.domStorageEnabled = true
@@ -16,12 +19,14 @@ class BlackActivity : AppCompatActivity(R.layout.activity_black) {
             webBlack.settings.userAgentString + "MobileAppClient/Android/0.9"
         webBlack.webViewClient = Client()
 
-        if (PreferenceProvider.getLastURL() == "") {
-            var url = PreferenceProvider.getUrl()
-            webBlack.loadUrl(url)
-        } else {
-            var url = PreferenceProvider.getLastURL()
-            webBlack.loadUrl(url)
+        if (savedInstanceState == null) {
+            if (PreferenceProvider.getLastURL() == "") {
+                var url = PreferenceProvider.getUrl()
+                webBlack.loadUrl(url)
+            } else {
+                var url = PreferenceProvider.getLastURL()
+                webBlack.loadUrl(url)
+            }
         }
     }
 
@@ -31,5 +36,15 @@ class BlackActivity : AppCompatActivity(R.layout.activity_black) {
         } else {
             super.onBackPressed()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webBlack.saveState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        webBlack.restoreState(savedInstanceState)
     }
 }
--- app/src/main/java/com/dat/android/gamebit/presentation/black/Client.kt
+++ app/src/main/java/com/dat/android/gamebit/presentation/black/Client.kt
@@ -1,11 +1,16 @@
 package com.dat.android.gamebit.presentation.black
 
+import android.content.Intent
+import android.net.Uri
 import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.dat.android.gamebit.App
 import com.dat.android.gamebit.utils.PreferenceProvider
 
 class Client : WebViewClient() {
+    var lastUrl =""
+
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
         CookieManager.getInstance().setAcceptCookie(true)
@@ -14,8 +19,24 @@ class Client : WebViewClient() {
     }
 
     override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-        PreferenceProvider.setLastURL(url!!)
-        view!!.loadUrl(url!!)
+        if (url!!.startsWith("mailto")) {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "plain/text"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(url!!.replace("mailto:", "")))
+            App.getInstance().startActivity(Intent.createChooser(intent, "Mail to Support"))
+        } else if (url!!.startsWith("tel:")) {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse(url)
+            App.getInstance().startActivity(intent)
+        } else if (url!!.startsWith("https://t.me/joinchat")) {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            App.getInstance().startActivity(intent)
+        }else {
+            PreferenceProvider.setLastURL(url!!)
+            view!!.loadUrl(url!!)
+        }
         return true
     }
+
+
 }
