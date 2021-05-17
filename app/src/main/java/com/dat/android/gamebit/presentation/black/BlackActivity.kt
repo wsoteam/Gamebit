22e54aa9d3fd4f00e5072b589a3cbd789aa5edab
Wadim <40245987+vsalamov@users.noreply.github.com>
2021-05-17 13:10:24 +0300

    web view add prog

--- app/src/main/java/com/dat/android/gamebit/presentation/black/BlackActivity.kt
+++ app/src/main/java/com/dat/android/gamebit/presentation/black/BlackActivity.kt
@@ -2,15 +2,21 @@ package com.dat.android.gamebit.presentation.black
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.FrameLayout
 import com.dat.android.gamebit.utils.PreferenceProvider
 import com.dat.android.gamebit.R
+import com.dat.android.gamebit.sound.SoundManager
 import com.dat.android.gamebit.utils.notification.NotifManager
 import kotlinx.android.synthetic.main.activity_black.*
 
 class BlackActivity : AppCompatActivity(R.layout.activity_black) {
+
+    lateinit var webBlack : WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        createUI()
         NotifManager.setAlarm(this)
 
         webBlack.settings.javaScriptEnabled = true
@@ -30,6 +36,15 @@ class BlackActivity : AppCompatActivity(R.layout.activity_black) {
         }
     }
 
+    private fun createUI() {
+        //Создаем программно веб вью
+        webBlack = WebView(this)
+        // Ставим в веб вью минимально необходимые параметры (высота и ширина). Тут важно тип соблюдать. Если FrameLayout то его и берем
+        webBlack.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+        // Добавляем к родителю
+        llParent.addView(webBlack)
+    }
+
     override fun onBackPressed() {
         if (webBlack.canGoBack()) {
             webBlack.goBack()
@@ -46,5 +61,10 @@ class BlackActivity : AppCompatActivity(R.layout.activity_black) {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         webBlack.restoreState(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        SoundManager.stop()
     }
 }
